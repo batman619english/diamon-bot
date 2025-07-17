ae2e1fc046f00a2d329ee9ae3aa9af62fa939a0c
batman619english <locker-soggy8x@icloud.com>
2025-07-17 16:50:13 -0700

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
 from linebot.models import (
     MessageEvent, TextMessage, TextSendMessage,
-    SourceGroup, Mention, Mentionee
+    SourceGroup, Mentionee
 )
 
 # === Replace these with your real token/secret ===
